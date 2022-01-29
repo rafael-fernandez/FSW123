3aa660b7982031df58e95a70a542546b045c5e2e
rafael-fernandez <rafael.fernandez@bryanuniversity.edu>
2022-01-28 20:41:49 -0700

    Week1 files

---
+++ week1/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import TodoList from './TodoList'
+
+
+
+function App() {
+
+  return (
+    <>
+      <TodoList />
+   <input type="checkbox"/>goto gym
+   <br></br> 
+   
+
+    <input type="checkbox"/>do laundry
+   <br></br> 
+
+    <input type="checkbox"/>food shopping 
+   <br></br> 
+
+  <input type="checkbox"/>do homerwork
+
+  <h4>FSW-123 Week1 Rafael Fernandez</h4>
+</>
+  );
+}
+
+export default App;
---
+++ week1/src/TodoList.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+
+
+ function TodoList() {
+  return <div>
+   <h1>List of Todos</h1>
+
+  </div>;
+  
+}
+
+export default TodoList
+
+
