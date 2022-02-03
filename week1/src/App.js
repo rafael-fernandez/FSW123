f32a05658ccd3beb6ab6d04e1046bc4fb5243713
Rafael.Fernandez <88115188+rafael-fernandez@users.noreply.github.com>
2022-02-02 19:59:07 -0700

    Update App.js

--- week1/src/App.js
+++ week1/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import TodoList from './TodoList'
+import "./styles.css";
 
 
 
@@ -7,20 +7,7 @@ function App() {
 
   return (
     <>
-      <TodoList />
-   <input type="checkbox"/>goto gym
-   <br></br> 
-   
-
-    <input type="checkbox"/>do laundry
-   <br></br> 
-
-    <input type="checkbox"/>food shopping 
-   <br></br> 
-
-  <input type="checkbox"/>do homerwork
-
-  <h4>FSW-123 Week1 Rafael Fernandez</h4>
+      <TodoList /> 
 </>
   );
 }
