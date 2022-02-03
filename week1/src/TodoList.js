6a91ffa77fb29d09f09abca55caf18ec9b46819c
Rafael.Fernandez <88115188+rafael-fernandez@users.noreply.github.com>
2022-02-02 20:00:26 -0700

    Update TodoList.js

--- week1/src/TodoList.js
+++ week1/src/TodoList.js
@@ -1,16 +1,23 @@
 
-import React from 'react';
-
 
 
  function TodoList() {
   return <div>
    <h1>List of Todos</h1>
+   <ul>
+  <li><input type="checkbox"/> goto gym</li>
+   <li><input type="checkbox"/> do laundry</li>
+   <li><input type="checkbox"/> food shopping</li>
+   <li><input type="checkbox"/> do homework</li>
+   </ul>
 
+    <h4>FSW-123 Week1 Rafael Fernandez</h4>
   </div>;
   
 }
 
 export default TodoList
+
+
 
 
