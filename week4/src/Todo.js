6299b0116b18e69e077b22ffca0fa8f63d21b926
Rafael.Fernandez <88115188+rafael-fernandez@users.noreply.github.com>
2022-02-21 20:55:54 -0700

    Assignment: ToDo Part 4 - Adding

---
+++ week4/src/Todo.js
@@ -0,0 +1,11 @@
+
+
+export default function Todo(props) {
+    return(
+        <li id={props.text.key} style={{textDecoration: props.text.isCompleted ? "line-through" : "" }}>
+            <input type="checkbox" onChange={() => props.completeToDo(props.text, props.text.id)}></input>
+            <label>{props.text.text}</label>
+            <button className="btn" onClick={() => props.deleteToDo(props.text.id)}>X</button>
+        </li>
+    )
+}
