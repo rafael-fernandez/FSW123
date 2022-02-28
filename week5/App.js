95f711788cfbb44b978ad4b9cc89e0f8346b404f
Rafael.Fernandez <88115188+rafael-fernandez@users.noreply.github.com>
2022-02-27 18:41:55 -0700

    Giphy Search

---
+++ week5/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SearchGiphyPage from './pages/SearchGiphyPage';
+
+function App() {
+
+  
+  return (
+    <SearchGiphyPage render = {"I did it!"}/>
+  )
+}
+
+export default App;
